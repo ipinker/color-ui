df50cc4b3ebd4289707049f52ce55f0dedebfafb
ipink <1242849166@qq.com>
2023-08-11 20:33:07 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-// import "test-publish/dist/style.css";
-// import ColorUI from "test-publish";
+// import "test-publish-colorui/dist/style.css";
+// import ColorUI from "test-publish-colorui";
 import { createSSRApp } from "vue";
 import App from "./App.vue";
 import ColorUI from "../color-ui/dist";
@@ -7,14 +7,14 @@ import "./static/icon/iconfont.css";
 import "../color-ui/dist/style.css";
 import store, { Pinia } from "./store";
 export function createApp() {
-	
+
     const app = createSSRApp(App);
-	
+
     app.use(store);
-	
+
     // 需要放置到
     ColorUI.install(app, {});
-	
+
     return {
         app,
         Pinia // 此处必须将 Pinia 返回
